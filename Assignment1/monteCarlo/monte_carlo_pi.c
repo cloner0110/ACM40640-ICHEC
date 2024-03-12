8314d38d407fdd0063df9f25b9f9778ff72163ae
Amirhossein Taran <det.em.tw@gmail.com>
2024-03-12 19:36:48 +0000

    adding comments to source code

--- Assignment1/monteCarlo/monte_carlo_pi.c
+++ Assignment1/monteCarlo/monte_carlo_pi.c
@@ -15,27 +15,44 @@
 #include <omp.h>
 #include "ran2.c"
 int main (){
-    float randNumberx, randNumbery, final_pi;
-    //- number of random generations
-    int i,tid;
-    int randGenLim = 100000;
+    //- x,y random numbers, estimated pi
+    float randNumberx, randNumbery, final_pi; 
+    
+    //- loop counter
+    int i;
+    
+    //- Total sample size
+    int sampleSize = 100000;
+    
+    //- samples hit the circle
     int nd = 0;
-	double time;
+    
+    //- access to time for measurements
+    double time;
+    time = omp_get_wtime();
+    
+    //- seed for ran2()
     long seed;
-	time = omp_get_wtime();
+
+    //-setting number of threads
     omp_set_num_threads(4);
-	#pragma omp parallel for private (i,randNumberx, randNumbery, seed) reduction(+:nd)
-	for (i=0; i<randGenLim; i++){
-        seed = i;
-		randNumberx = ran2(&seed);
-		randNumbery = ran2(&seed);
-	 	if ((pow((randNumberx-0.5),2)+pow((randNumbery-0.5),2)) < 0.25){
-	 		nd++;
-	 }
-	}
-	final_pi=4.0*( (double)nd/(double)randGenLim);
-	printf("Pi = %20.16f \n",final_pi);	
-	printf("Time: %f \n",omp_get_wtime()-time);
+    
+    //- starting parallel region
+    #pragma omp parallel for private (i,randNumberx, randNumbery, seed) reduction(+:nd)
+    for (i=0; i<sampleSize; i++){
+    //- setting seed based on loop counter
+    seed = i;
+    randNumberx = ran2(&seed);
+    randNumbery = ran2(&seed);
+
+    //- if condition for counting hits => nd
+    if ((pow((randNumberx-0.5),2)+pow((randNumbery-0.5),2)) < 0.25){
+        nd++;
+    }
+    }   
+final_pi=4.0*((double)nd/(double)sampleSize);
+printf("Pi = %1.16f \n",final_pi);	    
+printf("Time: %1.6f \n",omp_get_wtime()-time);
 return 0;
-	}
+}
 
