73687731f454ea3f2d431850a352735e48c9dc90
Amirhossein Taran <det.em.tw@gmail.com>
2024-03-19 18:51:20 +0000

    fixed minor bugs, added a time averaging section, TODO : serial code to be added !

--- Assignment1/steadyState/steadyState.c
+++ Assignment1/steadyState/steadyState.c
@@ -26,8 +26,9 @@ int getindex(struct matrix m, int i, int j);
 
 int main(void) {
     int i,j,k,u,nthreads;
-    int M = 100 ;
-    int N = 100 ;
+    int nSamples = 2;
+    int M = 300 ;
+    int N = 300 ;
     unsigned iter;
     struct matrix w_mat, w_mat_new;
     double conv_tolerance = 1E-04;
@@ -37,108 +38,111 @@ int main(void) {
     w_mat.ncol = N;
     w_mat_new.nrow = M;
     w_mat_new.ncol = N;
-    for (u=0; u<5; u++)
-    {
-        for (nthreads=1 ; nthreads <=4; nthreads +=1)
-            {
-                if (nthreads ==10)
-                    {
-                        nthreads = 16;
-                    }
+    double *times = (double *) malloc(nSamples*sizeof(double));
+    double tmin=DBL_MAX;
+    double tmax=0.0;
+    for (nthreads=1 ; nthreads <=4; nthreads +=1){
+    
         omp_set_num_threads(nthreads);
-        
-/* Assign space */
-    w_mat.matrix = (float *)malloc(w_mat.nrow * w_mat.ncol*sizeof(float));
-    w_mat_new.matrix = (float *)malloc(w_mat_new.nrow * w_mat_new.ncol*sizeof(float));
-
-    startTime = omp_get_wtime();
-#pragma omp parallel default(shared) private(i,j,err)
-{
-/* Initialization */
-    #pragma omp for schedule (static) collapse(2)
-        for (i=0 ; i<M ; i++)
+        for (u=1; u<=10; u++){
+        /* Assign space */
+        w_mat.matrix = (float *)malloc(w_mat.nrow * w_mat.ncol*sizeof(float));
+        w_mat_new.matrix = (float *)malloc(w_mat_new.nrow * w_mat_new.ncol*sizeof(float));
+        times[u-1] = omp_get_wtime();
+        #pragma omp parallel default(shared) private(i,j,err)
+        {
+            /* Initialization */
+            #pragma omp for schedule (static) collapse(2)
+            for (i=0 ; i<M ; i++)
+                for (j=0 ; j<N ; j++)
+                    {
+                        w_mat.matrix[getindex(w_mat,i,j)]=75.0;
+                        w_mat_new.matrix[getindex(w_mat_new,i,j)]=75.0;
+                    }
+            //- First row
+            #pragma omp for nowait schedule(static)
+            for (j=1; j<N-1; j++)
+            {
+                w_mat.matrix[getindex(w_mat,0,j)] = 0.0;
+                w_mat_new.matrix[getindex(w_mat_new,0,j)] = 0.0;
+            }
+            //- Last row
+            #pragma omp for nowait schedule(static)
             for (j=0 ; j<N ; j++)
-                {
-                    w_mat.matrix[getindex(w_mat,i,j)]=75.0;
-                    w_mat_new.matrix[getindex(w_mat_new,i,j)]=75.0;
+            {
+                w_mat.matrix[getindex(w_mat,N-1,j)]=100.0;
+                w_mat_new.matrix[getindex(w_mat_new,N-1,j)]=100.0;
+            }
+            //- First column
+            #pragma omp for nowait schedule(static)
+            for (i=0 ; i<M ; i++)
+            {
+                w_mat.matrix[getindex(w_mat,i,0)]=100.0;
+                w_mat_new.matrix[getindex(w_mat_new,i,0)]=100.0;
+            }
+            //- Last column
+            #pragma omp for nowait schedule(static)
+            for (i=0 ; i<M ; i++)
+            {
+                w_mat.matrix[getindex(w_mat,i,N-1)]=100.0;
+                w_mat_new.matrix[getindex(w_mat_new,i,N-1)]=100.0;
+            }
+            //- Internal Values
+            iter = 0;
+            max_err = DBL_MAX;
+            //- iteration (solution) loop
+            while(max_err > conv_tolerance)
+            {
+                ++iter;
+                #pragma omp for schedule(static) collapse(2)
+                for (i=1 ; i<M-1 ; i++){
+                    for (j=1 ; j<N-1 ; j++){
+                        w_mat_new.matrix[getindex(w_mat_new,i,j)] =
+                            0.25*
+                            (
+                            w_mat.matrix[getindex(w_mat,i+1,j)] +
+                            w_mat.matrix[getindex(w_mat,i-1,j)] +
+                            w_mat.matrix[getindex(w_mat,i,j+1)] +                            w_mat.matrix[getindex(w_mat,i,j-1)]
+                            );
+                    }
                 }
-        //- First row
-        #pragma omp for nowait schedule(static)
-        for (j=1; j<N-1; j++)
-        {
-            w_mat.matrix[getindex(w_mat,0,j)] = 0.0;
-            w_mat_new.matrix[getindex(w_mat_new,0,j)] = 0.0;
-        }
-        //- Last row
-        #pragma omp for nowait schedule(static)
-        for (j=0 ; j<N ; j++)
-        {
-            w_mat.matrix[getindex(w_mat,N-1,j)]=100.0;
-            w_mat_new.matrix[getindex(w_mat_new,N-1,j)]=100.0;
-        }
-        //- First column
-        #pragma omp for nowait schedule(static)
-        for (i=0 ; i<M ; i++)
-        {
-            w_mat.matrix[getindex(w_mat,i,0)]=100.0;
-            w_mat_new.matrix[getindex(w_mat_new,i,0)]=100.0;
-        }
-        //- Last column
-        #pragma omp for nowait schedule(static)
-        for (i=0 ; i<M ; i++)
-        {
-            w_mat.matrix[getindex(w_mat,i,N-1)]=100.0;
-            w_mat_new.matrix[getindex(w_mat_new,i,N-1)]=100.0;
-        }
-        //- Internal Values
-
-iter = 0;
-max_err = DBL_MAX;
-//- iteration (solution) loop
-while(max_err > conv_tolerance)
-{
-        ++iter;
-    #pragma omp for schedule(static) collapse(2)
-    for (i=1 ; i<M-1 ; i++)
-		{
-			for (j=1 ; j<N-1 ; j++)
-			{
-				w_mat_new.matrix[getindex(w_mat_new,i,j)] =
-                        0.25*
-                        (
-                        w_mat.matrix[getindex(w_mat,i+1,j)] +
-                        w_mat.matrix[getindex(w_mat,i-1,j)] +
-                        w_mat.matrix[getindex(w_mat,i,j+1)] +
-                        w_mat.matrix[getindex(w_mat,i,j-1)]
+                max_err = 0.0;
+                #pragma omp for schedule(static) collapse(2)
+                for (i=1 ; i<M-1 ; i++){
+                    for (j=1 ; j<N-1 ; j++){
+                        err =fabs(
+                            w_mat_new.matrix[getindex(w_mat_new,i,j)] -
+                            w_mat.matrix[getindex(w_mat,i,j)]
                         );
-			}
-		}
-        max_err = 0.0;
-    #pragma omp for schedule(static) collapse(2)
-    for (i=1 ; i<M-1 ; i++)
-		{
-			for (j=1 ; j<N-1 ; j++)
-			{
-                err =fabs(
-                     w_mat_new.matrix[getindex(w_mat_new,i,j)] -
-                    w_mat.matrix[getindex(w_mat,i,j)]
-                );
-                if (err > max_err)
-                {
-                    max_err = err;
+                    if (err > max_err){
+                        max_err = err;
+                    }
+                    w_mat.matrix[getindex(w_mat,i,j)] =
+                    w_mat_new.matrix[getindex(w_mat_new,i,j)] ;
+                    }
                 }
-                w_mat.matrix[getindex(w_mat,i,j)] =
-                w_mat_new.matrix[getindex(w_mat_new,i,j)] ;
             }
         }
-}
-}
-double endTime = omp_get_wtime() - startTime;
-
-printf("Run Time (s) = %4.3lf for %d threads\n", endTime,nthreads);
-}
-		printf("\n");
-
+        times[u-1] = omp_get_wtime()-times[u-1];    
+        }
+    double timeAvg = 0.0;
+    for(i=0; i<nSamples; ++i) {
+        timeAvg+=times[i];
+    }
+    timeAvg/=nSamples;
+    double sig=0.0;
+    for(i=0; i<nSamples; ++i) {
+    sig+=(times[i]-timeAvg)*(times[i]-timeAvg);
+    }
+    sig=sqrt(sig/nSamples);
+    printf("\nSummary:\n");
+    printf("Number of Threads = %d \n",nthreads);
+    printf("|  Avg. Time (s) | sigma Time(s)\n");
+    printf("%16.8f %16.8f\n",timeAvg,sig);
+    }
+free(times);
+free(w_mat.matrix);
+free(w_mat_new.matrix);
 }
 
 
@@ -157,8 +161,7 @@ printf("Run Time (s) = %4.3lf for %d threads\n", endTime,nthreads);
 //         } 
 //         fprintf(file2, "\n"); 
 //     } 
- } 
-
+//} 
 
 int getindex(struct matrix m, int i, int j){
 
